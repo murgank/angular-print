416a749a75e9b019495b9dd9f8bce86c7e7c61f5
murgank <knn>
2020-06-22 14:31:06 +0530

    Add. Directive to print doc

---
+++ src/app/doc-print/doc-print.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DocPrintDirective } from './doc-print.directive';
+
+describe('DocPrintDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DocPrintDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/doc-print/doc-print.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDocPrint]'
+})
+export class DocPrintDirective {
+
+  constructor() { }
+
+}
