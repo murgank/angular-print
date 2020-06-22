2069730d77e748d35da1ff11724e3a5a06eebdeb
murgank <knn>
2020-06-22 14:39:01 +0530

    Add. Print a page on a click of a button capability

--- src/app/doc-print/doc-print.directive.spec.ts
+++ src/app/doc-print/doc-print.directive.spec.ts
@@ -1,8 +1,39 @@
 import { DocPrintDirective } from './doc-print.directive';
+import { TestBed } from "@angular/core/testing";
+import { Component } from "@angular/core";
+import { By } from "@angular/platform-browser";
+
+@Component({
+  template: `
+    <div id="id">Print Me</div>
+    <button class="button" doc-print printSectionId="id"></button>
+  `
+})
+class TestComponent {}
 
 describe('DocPrintDirective', () => {
+  let fixture, testComponentButton;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [DocPrintDirective, TestComponent]
+    }).createComponent(TestComponent);
+
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
     const directive = new DocPrintDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('should print', function() {
+    spyOn(window, 'print');
+    testComponentButton = fixture.debugElement.query(By.css('.button'));
+
+    testComponentButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(window.print).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/doc-print/doc-print.directive.ts
+++ src/app/doc-print/doc-print.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appDocPrint]'
+  selector: 'button[doc-print]'
 })
 export class DocPrintDirective {
+  @Input() printSectionId: string;
 
-  constructor() { }
+  @HostListener('click')
+  public print(): void {
+    const printContents = document.getElementById(this.printSectionId);
+    const parentNode = printContents.parentNode;
+    const headElement = document.getElementsByTagName('head')[0];
+    const style = document.createElement('style');
+    style.innerText = `@media print { #${this.printSectionId} ~ * { display: none; }}`;
 
+    headElement.prepend(style);
+    document.getElementsByTagName('body')[0].prepend(printContents);
+
+    window.print();
+
+    parentNode.appendChild(printContents);
+    headElement.removeChild(style);
+  }
 }
